f12612261ba9664b159cec490d41999b5d35c54d
alaahagag <36234540+alaahagag@users.noreply.github.com>
2020-08-12 03:18:46 +0200

    update

---
+++ kasr-EL ainy/js/history-main.js
@@ -0,0 +1,167 @@
+function SendJSON()
+{ 
+            
+            // ........for patient information........
+
+
+    // for radio buttons (info.html)
+    var sex = localStorage.getItem('sex_value');
+    var occupation = localStorage.getItem('occupation_value');
+    var activedriving = localStorage.getItem('driving_value');
+    var handedness = localStorage.getItem('hand_value');
+    var familyConsanguinity = localStorage.getItem('familyCons_value');
+    var familycondition = localStorage.getItem('familyCond_value');
+    var fabrile = localStorage.getItem('fabrile_value');
+
+    // for text inputs (info.html)
+    var fname = localStorage.getItem('first_name');
+    var mname = localStorage.getItem('middle_name');
+    var lname = localStorage.getItem('last_value');
+    var age = localStorage.getItem('age_value');
+    var idnum = localStorage.getItem('id_value');
+    var pnumber = localStorage.getItem('pnumber_value');
+  
+    // for textarea (info.html)
+    var SpecifyWorking = localStorage.getItem('SpecifyWorking_Value');
+    var medications = localStorage.getItem('medication_value');
+    var neurologicalcondition = localStorage.getItem('neuro_value');
+    var describefebrile = localStorage.getItem('describefebrile_value');
+    var ageofonset = localStorage.getItem('ageofonset_value');
+   
+    // for dropdown list (info.html)
+    var education = localStorage.getItem('education_value');
+    var maritalstatus = localStorage.getItem('maritalstatus_value');
+    var habitnames = localStorage.getItem('habitnames_value');
+   
+
+       // ..... for seziure information ......
+
+    // for radio buttons 
+    var symptom_appearance = localStorage.getItem('symptom_appearance_value');
+    var number = localStorage.getItem('number_value');
+    var history = localStorage.getItem('history_value');
+    var admition_to_hospital = localStorage.getItem('admition_to_hospital_value');
+    var type_of_seizure = localStorage.getItem('type_of_seizure_value');
+    var seizurecontrol = localStorage.getItem('seizurecontrol_value');
+    
+    // for text inputs 
+    var age_of_onset = localStorage.getItem('age_of_onset_value');
+    var duration = localStorage.getItem('duration_value');
+    var frequency_before_AED = localStorage.getItem('frequency_before_AED_value');
+    var frequency_after_AED = localStorage.getItem('frequency_after_AED_value');
+    var longest_period = localStorage.getItem('longest_period_value');
+    var precipitated = localStorage.getItem('precipitated_value');
+    var aed_on_discharge = localStorage.getItem('aed_on_discharge_value');
+    var durationofstay = localStorage.getItem('durationofstay_value');
+    var intubation = localStorage.getItem('intubation_value');
+    var brainlesionAge = localStorage.getItem('brainlesionAge_value');
+    
+  
+
+    // for textarea 
+    var semiology = localStorage.getItem('semiology_Value');
+   
+    // for dropdown list 
+    var mode = localStorage.getItem('mode_value');
+    var classification = localStorage.getItem('classification_value');
+    var generalizednonMotors = localStorage.getItem('generalizednonMotors_value');
+    var postictal = localStorage.getItem('postictal_value');
+    var etiology = localStorage.getItem('etiology_value');
+    var structural = localStorage.getItem('structural_value');
+    var focalMotors = localStorage.getItem('focalMotors_value');
+
+
+    // ........ for labs .......
+
+    var date =  document.getElementById("date").value;
+
+
+   
+
+
+
+   
+
+
+
+
+   
+
+   
+      // Creating a XHR object 
+            let xhr = new XMLHttpRequest(); 
+            let url = "submit.txt";
+        
+            // open a connection 
+            xhr.open("POST", url, true); 
+
+            // Set the request header i.e. which type of content you are sending 
+            xhr.setRequestHeader("Content-Type", "application/json"); 
+
+            // Create a state change callback 
+            xhr.onreadystatechange = function () { 
+                if (xhr.readyState === 4 && xhr.status === 200) { 
+
+                    // Print received data from server 
+                    //result.innerHTML = this.responseText;
+                    console.log(this.responseText);
+
+
+                } 
+            }; 
+
+
+
+            // Converting JSON data to string 
+            var data = JSON.stringify({  "fname": fname, "mname": mname ,
+                "lname" : lname  ,
+                "idnum" :idnum , "age":age,"pnumber" : pnumber ,
+                 "medications":medications,
+                "neurologicalcondition":neurologicalcondition,
+                "describefebrile":describefebrile, "ageofonset":ageofonset,
+                "sex":sex , "occupation":occupation, 
+                "activedriving":activedriving, "handedness":handedness,
+                "familyConsanguinity":familyConsanguinity , "familycondition":familycondition
+                ,"fabrile":fabrile , "education":education , "maritalstatus":maritalstatus
+                ,"habitnames":habitnames ,
+                  
+                "seizure":[{"age_of_onset":age_of_onset,
+                "mode":mode ,
+                "classification": [{"cname":classification}],
+                "focalMotors": [{"fmname":focalMotors}],
+                "generalizednonMotors": [{"gnname":generalizednonMotors}],
+                "semiology":semiology,
+               
+                "symptom_appearance":symptom_appearance,
+                "number":number,
+                "duration":duration,
+                "postictal":postictal,
+                
+                "frequency_before_AED":frequency_before_AED,
+                "frequency_after_AED":frequency_after_AED,
+                "longest_period": longest_period }] ,
+                
+
+                    "tests":[{
+                    "date":date}]
+
+                
+                
+
+
+
+             });
+
+
+
+            // Sending data with the request 
+            xhr.send(data);
+            console.log("true");
+            console.log(data);
+    
+        } 
+     
+
+
+            
+        
--- kasr-EL ainy/js/seizure-main.js
+++ kasr-EL ainy/js/seizure-main.js
@@ -1,97 +1,111 @@
 
-function SendJSON(){ 
-            
-    // for radio buttons
-    var sex = localStorage.getItem('sex_value');
-    var occupation = localStorage.getItem('occupation_value');
-    var activedriving = localStorage.getItem('driving_value');
-    var handedness = localStorage.getItem('hand_value');
-    var familyConsanguinity = localStorage.getItem('familyCons_value');
-    var familycondition = localStorage.getItem('familyCond_value');
-    var fabrile = localStorage.getItem('fabrile_value');
+function PassSeizureValue(){ 
 
-    // for text inputs
-    var fname = localStorage.getItem('first_name');
-    var mname = localStorage.getItem('middle_name');
-    var lname = localStorage.getItem('last_value');
-    var age = localStorage.getItem('age_value');
-    var idnum = localStorage.getItem('id_value');
-    var pnumber = localStorage.getItem('pnumber_value');
-    var age_of_onset =  document.getElementById("age_of_onset").value;
+    
+
+     // for radiobuttons
+    var symptom_appearanceValue = document.querySelector('input[name =OrderSymptom]:checked').value;
+    localStorage.setItem('symptom_appearance_value', symptom_appearanceValue  );
+
+    var numberValue = document.querySelector('input[name = NumberOfSeizure]:checked').value;
+    localStorage.setItem('number_value', numberValue  );
+
+    var historyValue = document.querySelector('input[name =HistoryOfProlongedSeizures]:checked').value;
+    localStorage.setItem('history_value', historyValue  );
+
+    var admition_to_hospitalValue = document.querySelector('input[name =HospitalAdmitted]:checked').value;
+    localStorage.setItem('admition_to_hospital_value', admition_to_hospitalValue  );
+
+    var type_of_seizureValue = document.querySelector('input[name =SeizureType]:checked').value;
+    localStorage.setItem('type_of_seizure_value', type_of_seizureValue   );
+
+    var seizurecontrolValue = document.querySelector('input[name = ControlleSeizure]:checked').value;
+    localStorage.setItem('seizurecontrol_value', seizurecontrolValue );
+
+     
+    // for text input  
+
+    var age_of_onsetValue =  document.getElementById("age_of_onset").value;
+    localStorage.setItem('age_of_onset_value', age_of_onsetValue  );
+
+    var durationValue =  document.getElementById("duration").value;
+    localStorage.setItem('duration_value', durationValue );
+
+    var frequency_before_AEDValue =  document.getElementById("frequency_before_AED").value;
+    localStorage.setItem('frequency_before_AED_value', frequency_before_AEDValue );
+
+    var frequency_after_AEDValue =  document.getElementById("frequency_after_AED").value;
+    localStorage.setItem('frequency_after_AED_value', frequency_after_AEDValue  );
+
+    var longest_periodValue =  document.getElementById("longest_period").value;
+    localStorage.setItem('longest_period_value', longest_periodValue );
+
+    var precipitatedValue =  document.getElementById("precipitated").value;
+    localStorage.setItem('precipitated_value', precipitatedValue );
+
+    var aed_on_dischargeValue =  document.getElementById("aed_on_discharge").value;
+    localStorage.setItem('aed_on_discharge_value', aed_on_dischargeValue );
+
+    var durationofstayValue =  document.getElementById("durationofstay").value;
+    localStorage.setItem('durationofstay_value', durationofstayValue );
+
+    var intubationValue =  document.getElementById("intubation").value;
+    localStorage.setItem('intubation_value', intubationValue );
+
+    var brainlesionAgeValue =  document.getElementById("brainlesionAge").value;
+    localStorage.setItem('brainlesionAge_value', brainlesionAgeValue );
+
+
 
 
     // for textarea 
-    var SpecifyWorking = localStorage.getItem('SpecifyWorking_Value');
-    var medications = localStorage.getItem('medication_value');
-    var neurologicalcondition = localStorage.getItem('neuro_value');
-    var describefebrile = localStorage.getItem('describefebrile_value');
-    var ageofonset = localStorage.getItem('ageofonset_value');
-   
-    // for dropdown list 
-    var education = localStorage.getItem('education_value');
-    var maritalstatus = localStorage.getItem('maritalstatus_value');
-    var habitnames = localStorage.getItem('habitnames_value');
-    var mode =document.querySelector('#mode').value; 
-    var classification =document.querySelector('#classification').value; 
-
-   
-
+    var semiologyValue =  document.getElementById("semiology").value;
+    localStorage.setItem('semiology_Value', semiologyValue );
 
 
 
    
 
-   
-      // Creating a XHR object 
-            let xhr = new XMLHttpRequest(); 
-            let url = "submit.txt";
-        
-            // open a connection 
-            xhr.open("POST", url, true); 
+    // for dropdown list
+     var modeValue =document.querySelector('#mode').value; 
+     localStorage.setItem('mode_value', modeValue );
 
-            // Set the request header i.e. which type of content you are sending 
-            xhr.setRequestHeader("Content-Type", "application/json"); 
+     var classificationValue =document.querySelector('#classification').value;
+     localStorage.setItem('classification_value', classificationValue );
+ 
+     var focalMotorsValue =document.querySelector('#focalMotors').value; 
+     localStorage.setItem('focalMotors_value', focalMotorsValue );
 
-            // Create a state change callback 
-            xhr.onreadystatechange = function () { 
-                if (xhr.readyState === 4 && xhr.status === 200) { 
+      var generalizednonMotorsValue =document.querySelector('#generalizednonMotors').value; 
+     localStorage.setItem('generalizednonMotors_value', generalizednonMotorsValue );
 
-                    // Print received data from server 
-                    //result.innerHTML = this.responseText;
-                    console.log(this.responseText);
+     var postictalValue =document.querySelector('#postictal').value; 
+     localStorage.setItem('postictal_value', postictalValue);
 
+     var etiologyValue =document.querySelector('#etiology').value; 
+     localStorage.setItem('etiology_value', etiologyValue);
 
-                } 
-            }; 
+     var structuralValue =document.querySelector('#structural').value; 
+     localStorage.setItem('structural_value', structuralValue);
 
-
-
-            // Converting JSON data to string 
-            var data = JSON.stringify({  "fname": fname, "mname": mname ,
-                "lname" : lname  ,
-                "idnum" :idnum , "age":age,"pnumber" : pnumber ,
-                "Specify-working":SpecifyWorking.value,"medications":medications,
-                "neurologicalcondition":neurologicalcondition,
-                "describefebrile":describefebrile, "ageofonset":ageofonset,
-                "sex":sex , "occupation":occupation, 
-                "activedriving":activedriving, "handedness":handedness,
-                "familyConsanguinity":familyConsanguinity , "familycondition":familycondition
-                ,"fabrile":fabrile , "education":education , "maritalstatus":maritalstatus
-                ,"habitnames":habitnames ,
-                "Seizure":[{"age_of_onset" : age_of_onset , "mode":mode , "classification":classification}]
-
-             });
-
-
-
-            // Sending data with the request 
-            xhr.send(data);
-            console.log("true");
-            console.log(data);
     
         } 
-     
+
+ 
 
 
-            
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
